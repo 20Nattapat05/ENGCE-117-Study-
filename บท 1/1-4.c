8524c0aba6b48e57e6e5898f7122e2ef27d75c94
_Nattapat.05 <127713233+20Nattapat05@users.noreply.github.com>
2025-11-24 17:48:54 +0700

    Update 1-4.c

--- บท 1/1-4.c
+++ บท 1/1-4.c
@@ -2,15 +2,10 @@
 
 int main() {
     int N, sum = 0;
-
-    printf("ป้อนตัวเลขจำนวนเต็ม N: ");
     scanf("%d", &N);
-
     for (int i = 1; i <= N; i++) {
         sum += i;
     }
-
-    printf("ผลรวมของตัวเลขตั้งแต่ 1 ถึง %d คือ: %d\n", N, sum);
-
+    printf("%d", sum);
     return 0;
 }
